76dc6506d6ec7326a14312558d6314f8fc8ee3e7
ettoc00 <ettore.cesari@yahoo.it>
2020-12-18 18:00:57 +0100

    Fixed setup

--- setup.py
+++ setup.py
@@ -19,27 +19,6 @@ setup(
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-=======
-  name = 'rethreader',
-  packages = ['rethreader'],
-  version = '1.0.1',
-  license='MIT',
-  description = 'Controlled multi-processing',
-  author = 'Ettore Cesari',
-  author_email = 'ettore.cesari@yahoo.it',
-  url = 'https://github.com/ettoc00/rethreader',
-  download_url = 'https://github.com/ettoc00/rethreader/archive/1.0.2.tar.gz',
-  keywords = ['thread', 'multi', 'processing'],
-  install_requires=[
-          'threading',
-      ],
-  classifiers=[  # Optional
-    # How mature is this project? Common values are
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
-    'Development Status :: 3 - Alpha',
->>>>>>> 2b49df7754a6045bbaf561a291e5d1858be63886
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
